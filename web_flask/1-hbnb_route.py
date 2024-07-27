414f13761d8e9283e024450e9da9c0ea39f180f1
yadsashel <ysalhi600@gmail.com>
2024-07-27 08:08:04 -0700

    Add script to start a Flask web application with / and /hbnb routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" This is a script that starts a Flask web application
+This application will have two routes:
+
+/: Displays "Hello HBNB!"
+/hbnb: Displays "HBNB" """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
