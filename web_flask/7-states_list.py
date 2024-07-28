f37d4da6c385c18a5ac5329243c47923b069696f
yadsashel <ysalhi600@gmail.com>
2024-07-28 05:37:00 -0700

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" This script start a Flask for Web app framework:
+
+web application must be listening on 0.0.0.0, port 5000
+must use storage for fetching data from the storage engine
+(FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()"""
+
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    return render_template('8-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
