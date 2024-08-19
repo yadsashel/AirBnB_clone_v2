24e10db28113f972548aa62a5e204354ccc034d4
yadsashel <ysalhi600@gmail.com>
2024-08-19 10:38:55 +0000

    the flask rans well

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,10 @@
-#!/usr/bin/python3
-""" This script starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_hbnb():
-    return 'Hello HBNB!'
-
+@app.route('/airbnb-onepage/')
+def hello_airbnb():
+    return 'Hello, Airbnb Onepage!'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
