3ea0b9893ac3c35c58fd7107077a268bd04e5314
yadsashel <ysalhi600@gmail.com>
2024-08-19 11:11:08 +0000

    everything ran well

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/airbnb-onepage/')
 def hello_airbnb():
-    return 'Hello, Airbnb Onepage!'
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
